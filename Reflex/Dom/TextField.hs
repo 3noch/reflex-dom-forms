2f7c4a8f80d464f4c0289dfb5aae0c204827592e
Elliot Cameron <eacameron@gmail.com>
2018-02-13 12:05:48 -0500

    Drop use of TH for mobile builds

--- Reflex/Dom/TextField.hs
+++ Reflex/Dom/TextField.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 
 module Reflex.Dom.TextField where
 
-import           Control.Lens    ((%~), (^.))
-import           Control.Lens.TH (makeLenses)
 import           Data.Default    (Default, def)
 import           Data.Map        (Map)
 import qualified Data.Map        as Map
@@ -16,12 +14,13 @@ import qualified Data.Text       as T
 import           GHC.Generics    (Generic)
 import           Reflex.Dom.Core
 
-
--- ^ Core parts to a text field, meant to be built by combinators.
 data TextFieldType
   = TextInputType !Text  -- ^ Text input with a given element type (e.g. @"text"@, @"date"@, etc.)
   | TextAreaType         -- ^ Text area with a given size in rows
 
+-- ^ Describes a text-based input, attributes, embellishments, etc.
+--
+-- You should build this type with 'def' and combinators instead of directly.
 data TextField t m = TextField{
   _textField_classes    :: ![Text],
   _textField_type       :: !TextFieldType,
@@ -32,49 +31,49 @@ data TextField t m = TextField{
   _textField_outsidePre :: !(m ()),  -- ^ Stuff appearing *before* the widget itself
   _textField_setValue   :: !(Event t Text)
   } deriving (Generic)
-makeLenses 'TextField
 
-instance (Applicative m, Reflex t) => Default (TextField t m) where def = TextField{
-  _textField_classes    = [],
-  _textField_type       = TextInputType "text",
-  _textField_initValue  = "",
-  _textField_attrs      = "required"=:"required",
-  _textField_insidePre  = pure (),
-  _textField_insidePost = pure (),
-  _textField_outsidePre = pure (),
-  _textField_setValue   = never
-  }
+instance (Applicative m, Reflex t) => Default (TextField t m) where
+  def = TextField{
+    _textField_classes    = [],
+    _textField_type       = TextInputType "text",
+    _textField_initValue  = "",
+    _textField_attrs      = "required"=:"required",
+    _textField_insidePre  = pure (),
+    _textField_insidePost = pure (),
+    _textField_outsidePre = pure (),
+    _textField_setValue   = never
+    }
 
 -- | Builds an @input@ element based on a 'TextField' configuration.
-mkField :: (DomBuilderSpace m ~ GhcjsDomSpace, DomBuilder t m, PostBuild t m) => TextField t m -> m (Dynamic t Text)
+mkField :: forall t m. (DomBuilderSpace m ~ GhcjsDomSpace, DomBuilder t m, PostBuild t m)
+        => TextField t m -> m (Dynamic t Text)
 mkField cfg = do
-  cfg^. textField_outsidePre
-  elAttr "div" ("class" =: T.intercalate " " ("ui input" : cfg^. textField_classes)) $ do
-    cfg^. textField_insidePre
+  _textField_outsidePre cfg
+  elAttr "div" ("class" =: T.intercalate " " ("ui input" : _textField_classes cfg)) $ do
+    _textField_insidePre cfg
 
-    textDyn <- case cfg^. textField_type of
-      TextInputType typ ->
-        _textInput_value <$> textInput (def
-          & textInputConfig_inputType    .~ typ
-          & textInputConfig_initialValue .~ cfg^. textField_initValue
-          & textInputConfig_attributes   .~ pure (cfg^. textField_attrs)
-          & textInputConfig_setValue     .~ cfg^. textField_setValue
-          )
-      TextAreaType ->
-        _textArea_value <$> textArea (def
-          & textAreaConfig_initialValue .~ cfg^. textField_initValue
-          & textAreaConfig_attributes   .~ pure (cfg^. textField_attrs)
-          & textAreaConfig_setValue     .~ cfg^. textField_setValue
-          )
+    textDyn <- case _textField_type cfg of
+      TextInputType typ -> _textInput_value <$> textInput (def
+        & textInputConfig_inputType    .~ typ
+        & textInputConfig_initialValue .~ _textField_initValue cfg
+        & textInputConfig_attributes   .~ pure (_textField_attrs cfg)
+        & textInputConfig_setValue     .~ _textField_setValue cfg
+        )
+      TextAreaType      -> _textArea_value <$> textArea (def
+        & textAreaConfig_initialValue .~ _textField_initValue cfg
+        & textAreaConfig_attributes   .~ pure (_textField_attrs cfg)
+        & textAreaConfig_setValue     .~ _textField_setValue cfg
+        )
 
-    cfg^. textField_insidePost
+    _textField_insidePost cfg
     pure textDyn
 
 
 addIcon :: (Applicative m) => Either (m ()) (m ()) -> TextField t m -> TextField t m
-addIcon mkIcon' cfg = cfg
-  & textField_classes    %~ (cls:)
-  & textField_insidePost %~ (*> mkIcon)
+addIcon mkIcon' cfg = cfg{
+  _textField_classes    = cls : _textField_classes cfg,
+  _textField_insidePost = _textField_insidePost cfg *> mkIcon
+  }
   where
     (cls, mkIcon) = case mkIcon' of
       Left  mkIcon_ -> ("left icon", mkIcon_)
@@ -83,19 +82,21 @@ addIcon mkIcon' cfg = cfg
 
 addInnerLabel :: (Applicative m) => Either (m ()) (m ()) -> TextField t m -> TextField t m
 addInnerLabel mkLabel' cfg = case mkLabel' of
-  Left  mkLabel -> cfg
-    & textField_classes   %~ ("labeled":)
-    & textField_insidePre %~ (*> mkLabel)
-  Right mkLabel -> cfg
-    & textField_classes    %~ ("right labeled":)
-    & textField_insidePost %~ (*> mkLabel)
+  Left  mkLabel -> cfg{
+    _textField_classes   = "labeled" : _textField_classes cfg,
+    _textField_insidePre = _textField_insidePre cfg *> mkLabel
+    }
+  Right mkLabel -> cfg{
+    _textField_classes    = "right labeled" : _textField_classes cfg,
+    _textField_insidePost = _textField_insidePost cfg *> mkLabel
+    }
 
 
 addLabel :: (Applicative m) => m () -> TextField t m -> TextField t m
-addLabel mkLabel = textField_outsidePre %~ (*> mkLabel)
+addLabel mkLabel cfg = cfg{ _textField_outsidePre = _textField_outsidePre cfg *> mkLabel }
 
 setFluid :: TextField t m -> TextField t m
-setFluid = textField_classes %~ ("fluid":)
+setFluid cfg = cfg{ _textField_classes = "fluid" : _textField_classes cfg }
 
 
 basicLabel :: DomBuilder t m => m a -> m a
@@ -105,48 +106,49 @@ tagLabel :: DomBuilder t m => m a -> m a
 tagLabel = elAttr "div" ("class"=:"ui tag label")
 
 setNumberType :: TextField t m -> TextField t m
-setNumberType = textField_type .~ TextInputType "number"
+setNumberType cfg = cfg{ _textField_type = TextInputType "number" }
 
 setDateType :: TextField t m -> TextField t m
-setDateType = textField_type .~ TextInputType "date"
+setDateType cfg = cfg{ _textField_type = TextInputType "date" }
 
 setPasswordType :: TextField t m -> TextField t m
-setPasswordType = textField_type .~ TextInputType "password"
+setPasswordType cfg = cfg{ _textField_type = TextInputType "password" }
 
 setEmailType :: TextField t m -> TextField t m
-setEmailType = textField_type .~ TextInputType "email"
+setEmailType cfg = cfg{ _textField_type = TextInputType "email" }
 
 setPhoneNumberType :: TextField t m -> TextField t m
-setPhoneNumberType = textField_type .~ TextInputType "tel"
+setPhoneNumberType cfg = cfg{ _textField_type = TextInputType "tel" }
 
 setTextAreaType :: Int -> TextField t m -> TextField t m
-setTextAreaType rows
-  = (textField_type  .~ TextAreaType)
-  . (textField_attrs %~ Map.insert "rows" (T.pack $ show rows))
+setTextAreaType rows cfg = cfg{
+  _textField_type  = TextAreaType,
+  _textField_attrs = Map.insert "rows" (T.pack $ show rows) (_textField_attrs cfg)
+  }
 
 setOptional :: TextField t m -> TextField t m
-setOptional = textField_attrs %~ Map.delete "required"
+setOptional cfg = cfg{ _textField_attrs = Map.delete "required" (_textField_attrs cfg) }
 
 setFieldName :: Text -> TextField t m -> TextField t m
-setFieldName name = textField_attrs %~ Map.insert "name" name
+setFieldName name cfg = cfg{ _textField_attrs = Map.insert "name" name (_textField_attrs cfg) }
 
 setPlaceholder :: Text -> TextField t m -> TextField t m
-setPlaceholder placeholder = textField_attrs %~ Map.insert "placeholder" placeholder
+setPlaceholder placeholder cfg = cfg{ _textField_attrs = Map.insert "placeholder" placeholder (_textField_attrs cfg) }
 
 setMin :: (Num a, Show a) => a -> TextField t m -> TextField t m
-setMin v = textField_attrs %~ Map.insert "min" (T.pack $ show v)
+setMin v cfg = cfg{ _textField_attrs = Map.insert "min" (T.pack $ show v) (_textField_attrs cfg) }
 
 setMax :: (Num a, Show a) => a -> TextField t m -> TextField t m
-setMax v = textField_attrs %~ Map.insert "max" (T.pack $ show v)
+setMax v cfg = cfg{ _textField_attrs = Map.insert "max" (T.pack $ show v) (_textField_attrs cfg) }
 
 setStep :: (Num a, Show a) => a -> TextField t m -> TextField t m
-setStep v = textField_attrs %~ Map.insert "step" (T.pack $ show v)
+setStep v cfg = cfg{ _textField_attrs = Map.insert "step" (T.pack $ show v) (_textField_attrs cfg) }
 
 setInitial :: Text -> TextField t m -> TextField t m
-setInitial = (textField_initValue .~)
+setInitial val0 cfg = cfg{ _textField_initValue = val0 }
 
 setChangeEvent :: Event t Text -> TextField t m -> TextField t m
-setChangeEvent = (textField_setValue .~)
+setChangeEvent setVal cfg = cfg{ _textField_setValue = setVal }
 
 -- Common embellishments
 addPrefixLabel :: DomBuilder t m => m () -> TextField t m -> TextField t m
